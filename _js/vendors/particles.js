fd05f8abcfdc0f88ea1f4ba8c0826e30738880cb
Hoàng Kim Phú <61643034+w4rf0t@users.noreply.github.com>
2022-08-30 18:32:39 +0700

    Update particles.js

--- _js/vendors/particles.js
+++ _js/vendors/particles.js
@@ -40,7 +40,7 @@ var pJS = function(tag_id, params) {
           nb_sides: 5
         },
         image: {
-          src: '',
+          src: './dollar-symbol.png',
           width: 100,
           height: 100
         }
